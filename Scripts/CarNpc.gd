a91fbd698b2716e7b3fea6124c5044f5938addd0
firipu123 <46404602+firipu123@users.noreply.github.com>
2024-10-03 23:36:47 +0200

    Gjort textures för staden

--- Scripts/CarNpc.gd
+++ Scripts/CarNpc.gd
@@ -6,18 +6,18 @@ var speed = 7
 
 func _ready():
 	car_mesh.global_position = global_position
-	progress = 2
+	progress = 5
 
 func _process(delta):
-	if global_position.distance_to(car_mesh.global_position) < 8:
+	if global_position.distance_to(car_mesh.global_position) < 10:
 		progress += speed * delta
 	
 	var direction = car_mesh.global_position.direction_to(global_position)
 	
 	car_mesh.look_at(global_position)
 	if car_mesh.global_position.distance_to(global_position) > 5 and car_ray.get_collider() == null:
-		car_mesh.velocity = direction * speed
+		car_mesh.velocity = lerp(car_mesh.velocity,direction * speed,0.05)
 	else:
-		car_mesh.velocity = direction * (speed/2)
+		car_mesh.velocity = lerp(car_mesh.velocity,direction * (speed/2),0.05)
 	
 	car_mesh.move_and_slide()
